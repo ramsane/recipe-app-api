9cfe91c43e91421a41999042276b5aaabb52e2bb
ramsane <ramanareddysane20@gmail.com>
2019-03-23 00:46:25 +0530

    Added manage user end point

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,8 @@ from rest_framework import status
 
 # create create user url using reverse
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 # helper function to create some users for testing
@@ -65,3 +67,107 @@ class PublicUserApiTests(TestCase):
         ).exists()
 
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that the token is created for the user"""
+        # create the payload
+        payload = {'email': 'ram@gam.com', 'password': 'tese123'}
+        # create the user
+        create_user(**payload)
+        # authenticate the user with APIClient from rest framework
+        res = self.client.post(TOKEN_URL, payload)
+
+        # see if we have token in the response.
+        self.assertIn('token', res.data)
+        # see if status code is ok
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        # create the user
+        create_user(email='ram@gami.cpm', password='1234')
+        # authenticate with invalid credentials
+        res = self.client.post(TOKEN_URL,
+                               {'email': 'ram@gami.cpm', 'password': '123'})
+
+        # make sure that the token is not in the response
+        self.assertNotIn('token', res.data)
+        # check the status code
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that the token is not created when the user doesn't exists"""
+        payload = {'email': 'ram@gmail.com', 'password': 'dse'}
+        # get the response from the APIClient ( from rest_framework )
+        res = self.client.post(TOKEN_URL, payload)
+
+        # make sure that the token is not in the response
+        self.assertNotIn('token', res.data)
+        # check the 400 bad request
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that password and email are required"""
+        # response for missing password
+        res_no_password = self.client.post(TOKEN_URL,
+                                           {'email': 'ram@gmail.com',
+                                            'password': ''})
+
+        # make sure that the token is not in the response
+        self.assertNotIn('token', res_no_password.data)
+        # check the 400 bad request
+        self.assertEqual(res_no_password.status_code,
+                         status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API that requires user authentication"""
+
+    def setUp(self):
+        # create the user for this tests
+        self.user = create_user(
+            email='test@gmail.com',
+            password='test124',
+            name='name'
+        )
+        # create an APIClient for this test
+        self.client = APIClient()
+        # authentocate this user with force_authenticate
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retreiving profile for logged in user"""
+        # since we already authenticated user, we can just get his profile
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on thisi url"""
+        # get the response from post request directly.
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpassword'}
+
+        # get response from the pach request from the uesr who is authenticated
+        res = self.client.patch(ME_URL, payload)
+        # get the values from the updated database
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,3 +17,17 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authentcated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        # Because we have added authentication classes,
+        # the request have the user. It takes care of that.
+        # We can just load it from the request
+        return self.request.user
